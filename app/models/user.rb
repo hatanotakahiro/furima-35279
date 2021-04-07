425eef0707bbaeb056274ef171912b8bccaca51b
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-07 19:09:24 +0900

    ユーザー登録機能修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,15 @@ class User < ApplicationRecord
   with_options  presence: true do  
     validates :nickname
     # 全角（ひらがな、カタカナ、漢字）であるかのバリデーション
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字、ひらがな、カタカナ）で入力して下さい。'}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字、ひらがな、カタカナ）で入力して下さい。'}
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字、ひらがな、カタカナ）で入力して下さい。'} do  
+      validates :last_name
+      validates :first_name
+    end
     # カタカナであるかのバリデーション
-    validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+    with_options format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
     validates :birth_date
   end
 end
