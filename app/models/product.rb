7a9d9bd4a134ac959d1e3110480579254a3f0e0a
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-07 19:18:26 +0900

    ユーザー投稿機能修正3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ApplicationRecord
 
-  belongs_to :user
-  has_one_attached :image
-  has_one :order
+  # belongs_to :user
+  # has_one_attached :image
+  # has_one :order
 
-  with_options  presence: true do  
-    validates :title
-    validates :text
-    validates :category_id
-    validates :status_id
-    validates :ship_pay_id
-    validates :area_id
-    validates :delivery_day_id
-    validates :price
-  end
+  # with_options  presence: true do  
+  #   validates :title
+  #   validates :text
+  #   validates :category_id
+  #   validates :status_id
+  #   validates :ship_pay_id
+  #   validates :area_id
+  #   validates :delivery_day_id
+  #   validates :price
+  # end
 end
