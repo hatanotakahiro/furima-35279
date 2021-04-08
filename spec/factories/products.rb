81dea40a10bdd40369141dfce333ebda28eed910
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-08 18:52:39 +0900

    商品出品バリデーション実装

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :product do
-    
+    title {Faker::Lorem.sentence(max_length: 40)}
+    text {Faker::Lorem.sentence(max_length: 1000)}
+    image {Faker::Lorem.sentence}
+
+
+
+    association :user 
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
