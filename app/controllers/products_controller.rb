29c22d8e0780f2de54ce19bb7d858a2375bc8772
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-09 20:22:09 +0900

    商品編集機能の実装１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless @product.user == current_user
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+
   private
   def product_params
     params.require(:product).permit(:title, :text, :category_id, :status_id, :ship_pay_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
